25b9ad8f4d2df2e1b3153c3c4ae7768334a49117
Saulompr99 <57179314+Saulompr99@users.noreply.github.com>
2023-01-18 21:35:20 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -26,20 +26,31 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ejecutaComando("cmd.exe", "/c", "ver");
+        ejecutaComandoyComprueba("cmd.exe", "/c", "hostname");
+        ejecutaComandoyEspera("cmd.exe", "/c", "netstat");
+        ejecutaComandoDirectorio("ping", "8.8.8.8", "C:/");
+        buscayGuarda("\"saulo\"", "usuarios.txt", "users.txt");
     }
     
-    public void ejecutaComando(String comando, String argumentos, String metodo) {
+    public static void ejecutaComando(String comando, String argumentos, String metodo) {
         try {
             ProcessBuilder pb = new ProcessBuilder(comando, argumentos, metodo);
             Process p = pb.start();
             p.waitFor();
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            
+            String linea;
+            while((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InterruptedException ex) {
             System.out.println("El proceso ha sido interrumpido");
         }
     }
-    public void ejecutaComandoyComprueba(String comando, String argumentos, String metodo) {
+    public static void ejecutaComandoyComprueba(String comando, String argumentos, String metodo) {
         try {
             ProcessBuilder pb = new ProcessBuilder(comando, argumentos, metodo);
             Process p = pb.start();
@@ -47,52 +58,78 @@ public class Main {
                 System.out.println("Esperando...");
                 sleep(5000);
             }
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
         } catch (IOException | InterruptedException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public void ejecutaComandoyEspera(String comando, String argumentos, String metodo) {
+    public static void ejecutaComandoyEspera(String comando, String argumentos, String metodo) {
         try {
             ProcessBuilder pb = new ProcessBuilder(comando, argumentos, metodo);
             Process p = pb.start();
             sleep(5000);
             if(p.isAlive()) {
                 p.destroy();
+            } else {
+                BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    System.out.println(linea);
+                }
             }
         } catch (IOException | InterruptedException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public void ejecutaComandoDirectorio(String comando, String argumentos, String directorio) {
+    public static void ejecutaComandoDirectorio(String comando, String argumentos, String directorio) {
         try {
             ProcessBuilder pb = new ProcessBuilder(comando, argumentos);
             pb.directory(new File(directorio));
             Process p = pb.start();
             p.waitFor();
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
             System.out.println("El proceso se ha ejecutado en: " + pb.directory());
         } catch (IOException | InterruptedException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public void buscayGuarda(String palabra, String entrada, String salida) {
-        ProcessBuilder pb = new ProcessBuilder("find", "", entrada, salida);
+    public static void buscayGuarda(String palabra, String entrada, String salida) {
+        ProcessBuilder pb = new ProcessBuilder("find", palabra, entrada);
         File fSalida = new File(salida);
         
         try {
             Process p = pb.start();
+            p.waitFor();
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String linea;
             if(!fSalida.exists()) {
                 fSalida.createNewFile();
             }
-            OutputStreamWriter osw = new FileWriter(fSalida);
+            OutputStreamWriter osw = new FileWriter(fSalida, true);
             BufferedWriter bw = new BufferedWriter(osw);
             while((linea = br.readLine()) != null) {
                 bw.write(linea);
+                bw.newLine();
+                bw.flush();
             }
         } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
